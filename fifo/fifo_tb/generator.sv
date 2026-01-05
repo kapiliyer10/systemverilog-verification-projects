8996e1b727fe5e1133b74e0beac9f4067f6359b9
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-04 22:41:16 -0500

    Update generator.sv
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- fifo/fifo_tb/generator.sv
+++ fifo/fifo_tb/generator.sv
@@ -1,23 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/22/2025 05:20:45 PM
-// Design Name: 
-// Module Name: generator
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 `include "fifo_req.sv"
 `include "scoreboard.sv"
 class generator;
