3c85d5fcc872249f718377e75ca340ac85fa563f
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-04 22:41:49 -0500

    Update monitor_fifo.sv
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- fifo/fifo_tb/monitor_fifo.sv
+++ fifo/fifo_tb/monitor_fifo.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/22/2025 09:22:29 PM
-// Design Name: 
-// Module Name: monitor_fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 class monitor_fifo;
     virtual fifo_inf fr_inf;
     mailbox #(fifo_rsp) mon2scb_mb;
