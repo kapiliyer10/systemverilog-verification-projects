0783e81856936c3ff7dee2062ee95acb97d5b593
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-04 22:42:40 -0500

    Update environment.sv
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- fifo/fifo_tb/environment.sv
+++ fifo/fifo_tb/environment.sv
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/23/2025 09:48:24 AM
-// Design Name: 
-// Module Name: environment
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 `include "generator.sv"
 `include "driver.sv"
 `include "monitor_fifo.sv"
