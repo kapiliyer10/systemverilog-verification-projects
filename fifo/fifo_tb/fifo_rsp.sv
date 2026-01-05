aa6a8a2130e7833532a293228cb88afaeb291983
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-04 22:41:02 -0500

    Update fifo_rsp.sv
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- fifo/fifo_tb/fifo_rsp.sv
+++ fifo/fifo_tb/fifo_rsp.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/24/2025 12:04:43 PM
-// Design Name: 
-// Module Name: fifo_rsp
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 class fifo_rsp #(parameter WIDTH = 8);
 
     logic [WIDTH-1:0] data;
