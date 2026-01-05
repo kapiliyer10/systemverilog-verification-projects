916f2ca4899fb05f315ed606266101a4baaf120f
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-04 22:42:55 -0500

    Update testbench.sv
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- fifo/fifo_tb/testbench.sv
+++ fifo/fifo_tb/testbench.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/22/2025 03:06:58 PM
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 `include "environment.sv"
 
 module testbench;
