51a2f3a92438b9ea23af8e3fb2665f9cb0b4ed07
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:30:15 -0500

    Update hazard_unit.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/hazard_unit.v
+++ Riscv_RTL/hazard_unit.v
@@ -4,7 +4,7 @@ module hazard_unit (
     input  wire [4:0] if_id_rs1,
     input  wire [4:0] if_id_rs2,
     output wire stall,
-    output wire pc_write      // PC enable (if stall -> 0)
+    output wire pc_write     
 );
     // load-use hazard: if ID/EX is load and destination matches IF/ID rs1/rs2 then stall
     assign stall = id_ex_memread && ((id_ex_rd != 0) && ((id_ex_rd == if_id_rs1) || (id_ex_rd == if_id_rs2)));
