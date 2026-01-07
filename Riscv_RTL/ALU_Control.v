0c980c708ddd7d8e77f7c5f5e8f0dfd672b515a1
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:17:59 -0500

    Update ALU_Control.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/ALU_Control.v
+++ Riscv_RTL/ALU_Control.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/14/2025 09:19:56 PM
-// Design Name: 
-// Module Name: ALU_Control
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ALU_Control(
 input [1:0]alu_op,
 input [2:0]func3,
