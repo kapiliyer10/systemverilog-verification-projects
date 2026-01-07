63441bcfac38185e785e28f5bbf7f440d68a0b96
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:29:11 -0500

    Update hazard_unit.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/hazard_unit.v
+++ Riscv_RTL/hazard_unit.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/21/2025 09:10:08 AM
-// Design Name: 
-// Module Name: hazard_unit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module hazard_unit (
     input  wire id_ex_memread,
     input  wire [4:0] id_ex_rd,
