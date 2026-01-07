225634dadc709c18229407f44ac9dd97d8ef9b59
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:32:59 -0500

    Update if_id_reg.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/if_id_reg.v
+++ Riscv_RTL/if_id_reg.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/14/2025 09:50:13 PM
-// Design Name: 
-// Module Name: if_id_reg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module if_id_reg(
    input  wire clk,
     input  wire rst,
