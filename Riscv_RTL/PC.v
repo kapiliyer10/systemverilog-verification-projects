b27f9f7e4d7e365e01c4b1af2e4c7e6bd5aa8b38
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:20:17 -0500

    Update PC.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/PC.v
+++ Riscv_RTL/PC.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/07/2025 10:01:36 PM
-// Design Name: 
-// Module Name: PC
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module PC(
 input clk,rst,
 input [31:0] pc_next,
