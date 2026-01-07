83e9d437a6fc2a168ed47478ab95d52c2f271a92
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:33:11 -0500

    Update mem_wb_reg.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/mem_wb_reg.v
+++ Riscv_RTL/mem_wb_reg.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/15/2025 06:48:15 PM
-// Design Name: 
-// Module Name: mem_wb_reg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module mem_wb_reg (
     input  wire        clk,
     input  wire        rst,
