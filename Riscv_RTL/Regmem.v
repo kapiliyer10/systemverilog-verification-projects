17d36725a30c3a5c3a0eac5d8a82be1b9362282b
kapiliyer10 <kapil.iyer10@gmail.com>
2026-01-06 19:26:42 -0500

    Update Regmem.v
    Signed-off-by: kapiliyer10 <kapil.iyer10@gmail.com>

--- Riscv_RTL/Regmem.v
+++ Riscv_RTL/Regmem.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/09/2025 11:14:32 PM
-// Design Name: 
-// Module Name: Regmem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Regmem(
 input clk,we,
 input [4:0] rr1,
